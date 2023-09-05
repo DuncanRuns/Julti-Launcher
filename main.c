2a0c02a2ed87c8f975ce6f7a115937af43ddb6f4
Duncan <59705125+DuncanRuns@users.noreply.github.com>
2023-09-05 15:16:30 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <windows.h>
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    WIN32_FIND_DATA findFileData;
+    HANDLE hFind = FindFirstFile("Julti*.jar", &findFileData);
+
+    if (hFind == INVALID_HANDLE_VALUE) {
+        MessageBox(NULL, "No Julti jar was found!", "Error", MB_ICONERROR | MB_OK);
+        return 1;
+    }
+
+    char* filename = findFileData.cFileName;
+    FindClose(hFind);
+
+    char command[1024];
+    snprintf(command, sizeof(command), "javaw -jar \"%s\"", filename);
+
+    STARTUPINFO si = { sizeof(STARTUPINFO) };
+    PROCESS_INFORMATION pi;
+
+    // CreateProcess parameters for hiding the console window
+    si.dwFlags |= STARTF_USESHOWWINDOW;
+    si.wShowWindow = SW_HIDE;
+
+    if (CreateProcess(NULL, command, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    } else {
+        return 1;
+    }
+
+    return 0;
+}
